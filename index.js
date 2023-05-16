6907009f90e3a5783448cf2cdae363efc283fda4
roksana-barna <roksanabarna1803@gmail.com>
2023-05-17 01:40:32 +0600

    vercel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express =require('express');
 const cors = require('cors');
+ const jwt=require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -21,14 +22,43 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+const varifyJWT=(req,res,next)=>{
+  console.log('hitting jwt')
+  console.log(req.headers.authorization);
+  const authorization=req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true,message:'unauthorized access'})
+  }
+  const token =authorization.split(' ')[1];
+  console.log('token inside verify jwt',token);
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
+    if(error){
+      return res.status(401).send({error,message:'unauthorized access'})
+    }
+    req.decoded =decoded;
+    next()
+  })
+}
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    
 
     const serviceCollection=client.db('carDoctor').collection('services');
     const bookingCollection=client.db('carDoctor').collection('bookings');
+// jwt
+app.post('/jwt',(req,res)=>{
+  const user = req.body;
+  console.log(user)
+  const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+    expiresIn:"1hr"
+  });
+  res.send({token});
+
+})
+
+
     // service e  sob data gula pawar jnno
     app.get('/services', async(req,res)=>{
         const cursor =serviceCollection.find(); 
@@ -43,8 +73,8 @@ async function run() {
       res.send(result);
     });
     // bookings some data gula pawar jnno
-    app.get('/bookings',async(req,res)=>{
-      console.log(req.query.email);
+    app.get('/bookings',varifyJWT,async(req,res)=>{
+      // console.log(req.headers.authorization);
       let query = {};
       if(req.query?.email){
 
