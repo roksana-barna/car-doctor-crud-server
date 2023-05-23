5de2198b320fdb10746da8ac3de038df53b42bf9
roksana-barna <roksanabarna1803@gmail.com>
2023-05-23 21:28:48 +0600

    sorting

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express =require('express');
+const express = require('express');
 const cors = require('cors');
- const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const app =express();
-const port =process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
 
 // middleware
@@ -22,20 +22,20 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-const varifyJWT=(req,res,next)=>{
+const varifyJWT = (req, res, next) => {
   console.log('hitting jwt')
   console.log(req.headers.authorization);
-  const authorization=req.headers.authorization;
-  if(!authorization){
-    return res.status(401).send({error:true,message:'unauthorized access'})
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
   }
-  const token =authorization.split(' ')[1];
-  console.log('token inside verify jwt',token);
-  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(error,decoded)=>{
-    if(error){
-      return res.status(401).send({error,message:'unauthorized access'})
+  const token = authorization.split(' ')[1];
+  console.log('token inside verify jwt', token);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ error, message: 'unauthorized access' })
     }
-    req.decoded =decoded;
+    req.decoded = decoded;
     next()
   })
 }
@@ -43,77 +43,87 @@ const varifyJWT=(req,res,next)=>{
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    
 
-    const serviceCollection=client.db('carDoctor').collection('services');
-    const bookingCollection=client.db('carDoctor').collection('bookings');
-// jwt
-app.post('/jwt',(req,res)=>{
-  const user = req.body;
-  console.log(user)
-  const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
-    expiresIn:"1hr"
-  });
-  res.send({token});
 
-})
+    const serviceCollection = client.db('carDoctor').collection('services');
+    const bookingCollection = client.db('carDoctor').collection('bookings');
+    // jwt
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      console.log(user)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1hr"
+      });
+      res.send({ token });
+
+    })
 
 
     // service e  sob data gula pawar jnno
-    app.get('/services', async(req,res)=>{
-        const cursor =serviceCollection.find(); 
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/services', async (req, res) => {
+      const sort=req.query.sort;
+      // 100 takar nicher gula sort
+      const query ={price:{$lt:100}}
+      const options = {
+      // sort returned documents in ascending order by title (A->Z)
+      sort: {
+         "price": sort ==='asc'?1: -1
+         },
+      // Include only the `title` and `imdb` fields in each returned document
+    };
+      const cursor = serviceCollection.find(query,options);
+      const result = await cursor.toArray();
+      res.send(result);
     })
     // booking er data gula clinet thke ante
-    app.post('/bookings',async(req,res)=>{
-      const booking=req.body;
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
       console.log(booking);
       const result = await bookingCollection.insertOne(booking)
       res.send(result);
     });
     // bookings some data gula pawar jnno
-    app.get('/bookings',varifyJWT,async(req,res)=>{
+    app.get('/bookings', async (req, res) => {
       // console.log(req.headers.authorization);
       let query = {};
-      if(req.query?.email){
+      if (req.query?.email) {
 
       }
-      const result =await  bookingCollection.find(query).toArray();
+      const result = await bookingCollection.find(query).toArray();
       res.send(result)
     })
-    
-    
+
+
 
     // ekta document pawar jnno find kore/ekta id dekhanor jnno server e...jno ekta id k load kora jay checkout path diye
-    app.get ('/services/:id',async(req,res)=>{
-        const id = req.params.id;
-        const query = {_id:new ObjectId(id)}
-        const options ={
-            projection:{title:1,price:1,service_id:1,img:1},
-        };
-        const result = await serviceCollection.findOne(query,options);
-        res.send(result);
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const options = {
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
+      };
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
     })
     // delete
-    app.delete('/bookings/:id',async(req,res)=>{
-      const id =req.params.id;
-      const query = {_id:new ObjectId(id)}
-      const result =await bookingCollection.deleteOne(query);
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingCollection.deleteOne(query);
       res.send(result);
     })
     // update
-    app.patch("/bookings/:id",async(req,res)=>{
-      const id =req.params.id;
-      const filter ={_id:new ObjectId(id)};
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
       const updatedBooking = req.body;
       console.log(updatedBooking);
       const updateDoc = {
-        $set:{
-          status:updatedBooking.status
+        $set: {
+          status: updatedBooking.status
         },
       };
-      const result = await bookingCollection.updateOne(filter,updateDoc)
+      const result = await bookingCollection.updateOne(filter, updateDoc)
       res.send(result);
     })
 
@@ -130,9 +140,9 @@ app.post('/jwt',(req,res)=>{
 run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
-    res.send('doctor is running')
+app.get('/', (req, res) => {
+  res.send('doctor is running')
 })
- app.listen(port, ()=>{
-    console.log('car doctor is commming')
- })
+app.listen(port, () => {
+  console.log('car doctor is commming')
+})
